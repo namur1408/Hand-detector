1306f3de79761c0f3add3cd123e8bf104d613e1a
Oleh Androsiuk <UL0297971@edu.uni.lodz.pl>
2025-06-01 03:12:48 +0200

    tests for HandDetector.cpp

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 #include "HandDetector.h"
+#include "FaceDetection.h"
+#include "HSVMask.h"
 #include <opencv2/opencv.hpp>
+#include <filesystem>
+namespace fs = std::filesystem;
 const std::string kCascadePath = "../../Test/Resources/fist.xml";
 class HandDetectorTest : public ::testing::Test {
 protected:
@@ -15,4 +19,20 @@ TEST_F(HandDetectorTest, CascadeLoadedSuccessfully) {
     ASSERT_NO_THROW({
         HandDetector det(kCascadePath);
         });
+}
+TEST_F(HandDetectorTest, DetectDoesNotThrow) {
+    cv::Mat img = cv::Mat::zeros(480, 640, CV_8UC3);
+    cv::Mat dummyMask = cv::Mat::ones(480, 640, CV_8UC1) * 255;
+    detector->setMask(dummyMask);
+    ASSERT_NO_THROW({
+        detector->detect(img);
+        });
+    cv::Mat mask = detector->getMask();
+    EXPECT_FALSE(mask.empty());
+}
+TEST_F(HandDetectorTest, DebugModeToggle) {
+    detector->setDebugMode(true);
+    EXPECT_TRUE(detector->getDebugMode());
+    detector->setDebugMode(false);
+    EXPECT_FALSE(detector->getDebugMode());
 }
